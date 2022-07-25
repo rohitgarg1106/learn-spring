d1e796a78707b119d6a33c0561ab78e33335e6e9
Rohit Garg <rohit.garg@meesho.com>
2022-07-25 09:47:11 +0530

    Spring Setter - Injection for properties

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,26 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
+    private String name;
+    private int salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("CricketCoach: inside name setter: " + name);
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        System.out.println("CricketCoach: inside salary setter: " + salary);
+        this.salary = salary;
+    }
 
     public CricketCoach(){
         System.out.println("CricketCoach: inside no-args constructor");
