1bf78eb9910d07b547a5c761f87f588d5ec00929
Rohit Garg <rohit.garg@meesho.com>
2022-07-25 17:40:28 +0530

    @postcontruct + @predestroy

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //bean id = thatSillyCoach
 @Component
 public class TennisCoach implements Coach {
@@ -18,6 +21,16 @@ public class TennisCoach implements Coach {
         System.out.println("I am inside default constructor");
     }
 
+    @PostConstruct
+    public void doMyInitStuff(){
+        System.out.println(">> TennisCoach: inside of doMyInitStuff");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanUpStuff(){
+        System.out.println(">> TennisCoach: inside of doMyCleanUpStuff");
+    }
     @Override
     public String getDailyWorkout(){
         return "Practice smash for 50 minutes";
