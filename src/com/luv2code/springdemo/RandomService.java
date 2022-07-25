05329e31bdfb85166a23a628906410356bb88209
Rohit Garg <rohit.garg@meesho.com>
2022-07-25 17:26:12 +0530

    Random fortune service

--- src/com/luv2code/springdemo/RandomService.java
+++ src/com/luv2code/springdemo/RandomService.java
@@ -2,10 +2,20 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomService implements FortuneService{
+    String[] fortuneList = {
+            "You have a secret admirer",
+            "Look how far you've come",
+            "Be careful who you trust"
+    };
+
+private Random random = new Random();
     @Override
     public String getDailyFortune() {
-        return "Your future is in your hands";
+        int rnd = random.nextInt(fortuneList.length);
+        return fortuneList[rnd];
     }
 }
